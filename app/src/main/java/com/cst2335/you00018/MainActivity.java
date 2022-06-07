5c6aaffd401f112bff1f5668b525b32a1f6072bb
Weixin You <you00018@algonquinlive.com>
2022-06-07 12:28:38 -0400

    step 1

--- app/src/main/java/com/cst2335/you00018/MainActivity.java
+++ app/src/main/java/com/cst2335/you00018/MainActivity.java
@@ -1,8 +1,18 @@
 package com.cst2335.you00018;
 
+import static com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_LONG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button2);
+        btn.setOnClickListener(e -> handleBtnClick());
+        CompoundButton switchBtn = findViewById(R.id.switch1);
+        switchBtn.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked){
+                Snackbar.make(buttonView,getResources().getString(R.string.switch_on), LENGTH_LONG).setAction(getResources().getString(R.string.undo),click->buttonView.setChecked(!isChecked)).show();
+            }
+            else{
+                Snackbar.make(buttonView,getResources().getString(R.string.switch_off), LENGTH_LONG).setAction(getResources().getString(R.string.undo),click->buttonView.setChecked(!isChecked)).show();
+            }
+        });
     }
+
+    public void handleBtnClick(){
+        Context context = getApplicationContext();
+        CharSequence text = getString(R.string.toast_message);
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
 }
